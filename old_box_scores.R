1755d0cdf55b8ee136582e5ecde4553a636bf111
lbenz730 <luke.benz@yale.edu>
2020-02-19 22:27:33 -0500

    Historical Box Scores

---
+++ old_box_scores.R
@@ -0,0 +1,42 @@
+library(ncaahoopR)
+library(readr)
+
+seasons <- paste0(2002:2018, gsub("0\\.", "-", sprintf("%.2f", seq(.03, .19, 0.01))))
+seasons <- "2016-17"
+for(j in 1:length(seasons)) {
+  schedules <- dir(paste(seasons[j], "schedules", sep = "/"), full.names = T)
+  schedules_clean <- dir(paste(seasons[j], "schedules", sep = "/"), full.names = F)
+  n <- length(schedules)
+  for(i in 1:n) {
+    ### Read in Schedule
+    s <- read_csv(schedules[i])
+    n1 <- nrow(s)
+    box### Try to Scrape PBP
+    for(k in 1:n1) {
+      cat("Scraping Game", k, "of", n1, "for Team", i, "of", n, "(Season =", seasons[j], ")\n")
+      team <- gsub("_", " ", gsub("_schedule.csv", "", schedules_clean[i]))
+      file <- paste(seasons[j], "box_scores", gsub(" ", "_", team), paste0(s$game_id[k], ".csv"), sep = "/")
+      if(!file.exists(file)) {
+        box <- try(get_boxscore(s$game_id[k]))
+        box_team <- dict$ESPN_PBP[dict$ESPN == team]
+        box[box_team]
+        
+        if(class(box) != "try-error" & box_team %in% names(box)) {
+          ### Create Date Directory if Doesn't Exist
+          if(!dir.exists(paste(seasons[j], "box_scores", sep = "/"))) {
+            dir.create(paste(seasons[j], "box_scores", sep = "/")) 
+          }
+          if(!dir.exists(paste(seasons[j], "box_scores", gsub(" ", "_", team), sep = "/"))) {
+            dir.create(paste(seasons[j], "box_scores", gsub(" ", "_", team), sep = "/"))
+          }
+          df <- as.data.frame(box[[box_team]])
+          df$date <- s$date[k]
+          df$opponent <- s$opponent[k]
+          df$location <- s$location[k]
+          write_csv(df, file)
+        }
+       
+      }
+    }
+  }
+}
--- update.R
+++ update.R
@@ -52,4 +52,44 @@ while(date <= Sys.Date()) {
 write_csv(master_schedule, "2019-20/pbp_logs/master_schedule.csv")
 
 
+### Box Scores
+schedules <- dir(paste("2019-20/schedules", sep = "/"), full.names = T)
+schedules_clean <- dir(paste("2019-20/schedules", sep = "/"), full.names = F)
+n <- length(schedules)
+for(i in 1:n) {
+  ### Read in Schedule
+  s <- read_csv(schedules[i])
+  n1 <- nrow(s)
+  box### Try to Scrape PBP
+  for(k in 1:n1) {
+    cat("Scraping Game", k, "of", n1, "for Team", i, "of", n, "(Season =", seasons[j], ")\n")
+    team <- gsub("_", " ", gsub("_schedule.csv", "", schedules_clean[i]))
+    file <- paste(seasons[j], "box_scores", gsub(" ", "_", team), paste0(s$game_id[k], ".csv"), sep = "/")
+    if(!file.exists(file)) {
+      box <- try(get_boxscore(s$game_id[k]))
+      box_team <- dict$ESPN_PBP[dict$ESPN == team]
+      box[box_team]
+      
+      if(class(box) != "try-error" & box_team %in% names(box)) {
+        ### Create Date Directory if Doesn't Exist
+        if(!dir.exists(paste(seasons[j], "box_scores", sep = "/"))) {
+          dir.create(paste(seasons[j], "box_scores", sep = "/")) 
+        }
+        if(!dir.exists(paste(seasons[j], "box_scores", gsub(" ", "_", team), sep = "/"))) {
+          dir.create(paste(seasons[j], "box_scores", gsub(" ", "_", team), sep = "/"))
+        }
+        df <- as.data.frame(box[[box_team]])
+        df$date <- s$date[k]
+        df$opponent <- s$opponent[k]
+        df$location <- s$location[k]
+        write_csv(df, file)
+      }
+      
+    }
+  }
+}
+
+
+
+
 
