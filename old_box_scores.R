e7c67d6e8559e9b29b7109b3864df68d2f769327
lbenz730 <luke.benz@yale.edu>
2020-02-21 12:44:17 -0500

    UConn

--- old_box_scores.R
+++ old_box_scores.R
@@ -19,7 +19,7 @@ for(j in 1:length(seasons)) {
       file <- paste(seasons[j], "box_scores", gsub(" ", "_", team), paste0(s$game_id[k], ".csv"), sep = "/")
       if(!file.exists(file)) {
         box <- try(get_boxscore(s$game_id[k]))
-        box_team <- dict$ESPN_PBP[dict$ESPN == team]
+        box_team <- ifelse(team == "UConn", team, dict$ESPN_PBP[dict$ESPN == team])
         box[box_team]
         
         if(class(box) != "try-error" & box_team %in% names(box)) {
